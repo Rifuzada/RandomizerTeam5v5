28377e7f646071ad60530d43d29844287e3dbec6
Rifuzada <84077010+Rifuzada@users.noreply.github.com>
2021-09-12 07:58:54 -0300

    Update Randomizer5v5.py

--- Randomizer5v5.py
+++ Randomizer5v5.py
@@ -2,26 +2,39 @@
 import random
 
 #Chose the players
-top1 = str(input("Player 1: "))
-top2 = str(input("Player 2: "))
-jg1 = str(input("Player 3: "))
-jg2 = str(input("Player 4: "))
-mid1 = str(input("Player 5: "))
-mid2 = str(input("Player 6: "))
-adc1 = str(input("Player 7: "))
-adc2 = str(input("Player 8: "))
-sup1 = str(input("Player 9: "))
-sup2 = str(input("Player 10: "))
+p1 = str(input("Player 1: "))
+p2 = str(input("Player 2: "))
+p3 = str(input("Player 3: "))
+p4 = str(input("Player 4: "))
+p5 = str(input("Player 5: "))
+p6 = str(input("Player 6: "))
+p7 = str(input("Player 7: "))
+p8 = str(input("Player 8: "))
+p9 = str(input("Player 9: "))
+p10 = str(input("Player 10: "))
 
 #The players you has chosen ill be randomized in two teams, Top = Top laner, Jg = Jungler, Mid = Mid Laner, Adc = Ad Carry, Sup = Support.
-#The roles in a MOBA game. 
-#1-2 is the first and second team. 
+#The roles in a MOBA game.
+#1-2 is the first and second team.
 #Time = Team
-times = [top1, top2, jg1, jg2, mid1, mid2, adc1, adc2, sup1, sup2]
+times = [p1 ,p2 ,p3 ,p4 ,p5 ,p6 ,p7 ,p8, p9, p10]   
 random.shuffle(times)
 time1, time2 = times[:5], times[5:]
-
 #Print the two teams.
 print("O primeiro time é {}".format(time1))  #The first team is {}
 print("O Segundo time é {}".format(time2))   #The second team is {}
+#Chose the Roles
+#Which role each player will play
+lane1 = str(input('Que lane o {} jogará? '.format(p1)))
+lane2 = str(input('Que lane o {} jogará? '.format(p2)))
+lane3 = str(input('Que lane o {} jogará? '.format(p3)))
+lane4 = str(input('Que lane o {} jogará? '.format(p4)))
+lane5 = str(input('Que lane o {} jogará? '.format(p5)))
+lane6 = str(input('Que lane o {} jogará? '.format(p6)))
+lane7 = str(input('Que lane o {} jogará? '.format(p7)))
+lane8 = str(input('Que lane o {} jogará? '.format(p8)))
+lane9 = str(input('Que lane o {} jogará? '.format(p9)))
+lane10 = str(input('Que lane o {} jogará? '.format(p10)))
+#Print the lanes.
+print("As lanes ficaram separadas como, \n{} na lane {}\n{} na lane {}\n{} na lane {}\n{} na lane {}\n{} na lane {}\n{} na lane {}\n{} na lane {}\n{} na lane {}\n{} na lane {}\n{} na lane {}".format(p1, lane1, p2 , lane2, p3 , lane3, p4, lane4, p5,lane5,p6,lane6,p7,lane7,p8, lane8,p9,lane9,p10,lane10))
 #👊🏿
